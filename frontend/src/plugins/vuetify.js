66f70c1ba2a782b95615aa6d882a249928a73e92
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-04-10 16:25:32 -0300

    renaming

---
+++ frontend/src/plugins/vuetify.js
@@ -0,0 +1,12 @@
+import '@mdi/font/css/materialdesignicons.css'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+export const vuetify = createVuetify({
+    components,
+    directives,
+})
