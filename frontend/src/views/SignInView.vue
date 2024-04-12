b869373fc2c7eeb5021c9e9227190870ca216ab7
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-04-12 18:48:20 -0300

    init: sign-in view

--- frontend/src/views/SignInView.vue
+++ frontend/src/views/SignInView.vue
@@ -1,8 +1,37 @@
 <script setup>
-
 </script>
 
 <template>
+    <v-form class="form">
+        <h1 class="text-center mb-5">Sign-in</h1>
+        <v-text-field label="E-mail" v-model="email" type="email" />
+        <v-text-field label="Senha" v-model="password" type="password" />
+
+        <div class="form-actions">
+            <router-link to="/signup">
+                <v-btn>Cadastrar-se</v-btn>
+            </router-link>
+            <v-btn>Log-in</v-btn>
+        </div>
+    </v-form>
 </template>
 
-<style scoped></style>
+<style scoped>
+.form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-content: center;
+
+    margin: 20vh 30vw;
+}
+
+.form-actions {
+    display: flex;
+    justify-content: space-around;
+}
+
+.v-btn {
+    background-color: rgb(211, 211, 211);
+}
+</style>
