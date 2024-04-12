73ddb30edcc02fc9e487ba97bbcba40d7530bb8d
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-04-12 18:47:51 -0300

    feat: sign-(in&up) route addition)

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import HomeView from '../views/HomeView.vue';
+import HomeView from '@/views/HomeView.vue';
+import SignInView from '@/views/SignInView.vue';
+import SignUpView from '@/views/SignUpView.vue';
 
 export const routes = [
   { path: '/', name: 'home', component: HomeView },
+  { path: '/signin', name: 'sign-in', component: SignInView },
+  { path: '/signup', name: 'sign-up', component: SignUpView },
 ]
 
 export const router = createRouter({
