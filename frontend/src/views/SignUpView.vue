17b6763fd011cf6e4420e7580065eab9e29a0835
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-04-12 20:41:15 -0300

    init: sign-in view

--- frontend/src/views/SignUpView.vue
+++ frontend/src/views/SignUpView.vue
@@ -1,8 +1,43 @@
 <script setup>
+import { ref } from 'vue';
+
+const name = ref('')
+const email = ref('')
+const password = ref('')
+const password_confirmation = ref('')
 
 </script>
 
 <template>
+    <v-form class="form" @submit.prevent>
+        <h1 class="text-center mb-5">Sign-up</h1>
+        <v-text-field label="Nome" v-model="name" type="text" />
+        <v-text-field label="E-mail" v-model="email" type="email" />
+        <v-text-field label="Senha" v-model="password" type="password" />
+        <v-text-field label="Confirmação de senha" v-model="password_confirmation" type="password" />
+
+        <div class="form-actions">
+            <v-btn>Cadastrar-se</v-btn>
+        </div>
+    </v-form>
 </template>
 
-<style scoped></style>
+<style scoped>
+.form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-content: center;
+
+    margin: 20vh 30vw;
+}
+
+.form-actions {
+    display: flex;
+    justify-content: space-around;
+}
+
+.v-btn {
+    background-color: rgb(211, 211, 211);
+}
+</style>
