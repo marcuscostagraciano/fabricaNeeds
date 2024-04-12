1bc108a53d1a67c7493d2b8bc4adb6d9bd66c1be
marcuscostagraciano <marcusviniciusgraciano04@gmail.com>
2024-04-12 10:57:09 -0300

    init: sidebar

--- frontend/src/components/SideBar.vue
+++ frontend/src/components/SideBar.vue
@@ -1,8 +1,10 @@
 <script setup>
+import { routes } from '@/router';
 
 </script>
 
 <template>
+    <router-link v-for="route in routes" :to="route.path">{{ route.name }}</router-link>
 </template>
 
 <style scoped></style>
