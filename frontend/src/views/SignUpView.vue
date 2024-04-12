126d30425272c477c5ac2c1270a7d53e0026d56c
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-04-12 20:41:43 -0300

    feat: input validations

--- frontend/src/views/SignUpView.vue
+++ frontend/src/views/SignUpView.vue
@@ -6,15 +6,32 @@ const email = ref('')
 const password = ref('')
 const password_confirmation = ref('')
 
+const MIN_PASSWORD_LENGTH = 8
+
+const requiredRule = (value) => value || 'Campo obrigatório'
+const emailRule = [
+    requiredRule,
+    (v) => (/^\S+@\S+\.\S+$/.test(v)) || "Insira um e-mail válido.",
+]
+const passwordRule = [
+    requiredRule,
+    (v) => (v.length >= MIN_PASSWORD_LENGTH) || `A senha deve ter no mínimo ${MIN_PASSWORD_LENGTH} dígitos.`,
+]
+const passwordConfirmationRule = [
+    requiredRule,
+    (v) => v === password.value || "As senhas devem ser iguais.",
+]
+
 </script>
 
 <template>
     <v-form class="form" @submit.prevent>
         <h1 class="text-center mb-5">Sign-up</h1>
-        <v-text-field label="Nome" v-model="name" type="text" />
-        <v-text-field label="E-mail" v-model="email" type="email" />
-        <v-text-field label="Senha" v-model="password" type="password" />
-        <v-text-field label="Confirmação de senha" v-model="password_confirmation" type="password" />
+        <v-text-field label="Nome" :rules="[requiredRule]" v-model="name" type="text" />
+        <v-text-field label="E-mail" :rules="emailRule" v-model="email" type="email" />
+        <v-text-field label="Senha" :rules="passwordRule" v-model="password" type="password" />
+        <v-text-field label="Confirmação de senha" :rules="passwordConfirmationRule" v-model="password_confirmation"
+            type="password" />
 
         <div class="form-actions">
             <v-btn>Cadastrar-se</v-btn>
