1f3039ec3193ef503bd207e6cde0116f0ef0597b
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-04-11 21:52:43 -0300

    feat: navbar

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -3,6 +3,37 @@
 </script>
 
 <template>
+    <v-app-bar id="site-header">
+        <v-row>
+            <v-col>
+                <v-app-bar-nav-icon icon="mdi-menu" />
+            </v-col>
+            <v-col id="site-title">
+                <v-app-bar-title>fabricaNeeds</v-app-bar-title>
+            </v-col>
+            <v-col>
+                <!-- Placeholder for user operations (login and logout) -->
+                <v-app-bar-nav-icon icon="mdi-account" />
+            </v-col>
+        </v-row>
+
+    </v-app-bar>
 </template>
 
-<style scoped></style>
+<style scoped>
+#site-header {
+    background-color: #575555;
+    color: #fff;
+
+    display: flex;
+    text-align: center;
+}
+
+#site-title {
+    align-self: center;
+}
+
+#logout-btn {
+    background-color: #f57c0b;
+}
+</style>
