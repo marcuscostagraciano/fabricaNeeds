d4fc4eccdbd317178a12a2ef3d19eca50e6d0806
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-04-13 10:45:05 -0300

    feat: form validation

--- frontend/src/views/SignInView.vue
+++ frontend/src/views/SignInView.vue
@@ -1,17 +1,31 @@
 <script setup>
+import { computed, ref } from 'vue';
+
+import { emailRule, passwordLengthRule } from '@/utils/formValidationRules';
+
+const email = ref('')
+const password = ref('')
+
+const isFormValid = computed(() => {
+    return [
+        emailRule[1](email.value),
+        passwordLengthRule[1](password.value),
+    ].every(rule => rule === true);
+});
+
 </script>
 
 <template>
     <v-form class="form">
         <h1 class="text-center mb-5">Sign-in</h1>
-        <v-text-field label="E-mail" v-model="email" type="email" />
-        <v-text-field label="Senha" v-model="password" type="password" />
+        <v-text-field label="E-mail" :rules="emailRule" v-model="email" type="email" />
+        <v-text-field label="Senha" :rules="passwordLengthRule" v-model="password" type="password" />
 
         <div class="form-actions">
             <router-link to="/signup">
                 <v-btn>Cadastrar-se</v-btn>
             </router-link>
-            <v-btn>Log-in</v-btn>
+            <v-btn :disabled="!isFormValid">Log-in</v-btn>
         </div>
     </v-form>
 </template>
