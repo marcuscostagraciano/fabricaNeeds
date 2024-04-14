3f5942b77568b12f7995829f560f907ce0107be9
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-04-14 15:10:34 -0300

    refactor(routes): separated file for better organization

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,17 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import routesJSON from '@/data/routes.json'
+
 import HomeView from '@/views/HomeView.vue'
 import SignInView from '@/views/SignInView.vue'
 import SignUpView from '@/views/SignUpView.vue'
 import RegisterItemView from '@/views/RegisterItemView.vue'
 
-export const routes = [
-  { path: '/', name: 'home', pt_name: 'página inicial', component: HomeView, show: true },
-  { path: '/history', name: 'history', pt_name: 'histórico', component: HomeView, show: true },
-  { path: '/signin', name: 'sign-in', pt_name: 'login', component: SignInView, show: false },
-  { path: '/signup', name: 'sign-up', pt_name: 'cadastro', component: SignUpView, show: false },
-  { path: '/registerItem', name: 'register-item', pt_name: 'registrar itens', component: RegisterItemView, show: true },
-]
+export const routes = routesJSON.map(route => ({
+  ...route,
+  component: () => import(`@/views/${route.component}.vue`)
+}));
 
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
