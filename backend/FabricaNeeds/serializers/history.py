b6b0a051d402f0109d5accbdf2a808f95785b506
joao <db.fan.db@gmail.com>
2024-04-14 10:39:06 -0300

    fix: I fixed a letter that was wrong

--- backend/FabricaNeeds/serializers/history.py
+++ backend/FabricaNeeds/serializers/history.py
@@ -1,7 +1,7 @@
 from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
 
-from FabricaNeeds.models import history
+from FabricaNeeds.models import History
 
 class HistorySerializer(serializers.ModelSerializer):
     class Meta:
