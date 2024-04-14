3427c7e76760c05284f7104d3c7294b81bce7edd
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-04-14 15:00:10 -0300

    feat: item registration route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,14 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import HomeView from '@/views/HomeView.vue';
-import SignInView from '@/views/SignInView.vue';
-import SignUpView from '@/views/SignUpView.vue';
+import HomeView from '@/views/HomeView.vue'
+import SignInView from '@/views/SignInView.vue'
+import SignUpView from '@/views/SignUpView.vue'
+import RegisterItemView from '@/views/RegisterItemView.vue'
 
 export const routes = [
   { path: '/', name: 'home', pt_name: 'página inicial', component: HomeView, show: true },
   { path: '/history', name: 'history', pt_name: 'histórico', component: HomeView, show: true },
   { path: '/signin', name: 'sign-in', pt_name: 'login', component: SignInView, show: false },
   { path: '/signup', name: 'sign-up', pt_name: 'cadastro', component: SignUpView, show: false },
+  { path: '/registerItem', name: 'register-item', pt_name: 'registrar itens', component: RegisterItemView, show: true },
 ]
 
 export const router = createRouter({
