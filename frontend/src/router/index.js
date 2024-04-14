c2975717921ed650d8413b498c3ac7e3aa4bee99
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-04-14 15:13:10 -0300

    refactor(imports): got rid of useless imports

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,11 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import routesJSON from '@/data/routes.json'
 
-import HomeView from '@/views/HomeView.vue'
-import SignInView from '@/views/SignInView.vue'
-import SignUpView from '@/views/SignUpView.vue'
-import RegisterItemView from '@/views/RegisterItemView.vue'
-
 export const routes = routesJSON.map(route => ({
   ...route,
   component: () => import(`@/views/${route.component}.vue`)
