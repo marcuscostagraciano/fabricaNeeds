3370e164b85dce16012e1727b784d963ef57b6e4
Gianluca Starke <gianlucastarke@gmail.com>
2024-04-15 13:21:17 -0300

    style: remove comments from unused code

--- backend/FabricaNeeds/views/item.py
+++ backend/FabricaNeeds/views/item.py
@@ -1,14 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
 
 from FabricaNeeds.models import Item
-from FabricaNeeds.serializers import ItemSerializer#, CriarItemSerializer
+from FabricaNeeds.serializers import ItemSerializer
 
 
 class ItemViewSet(ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
-
-    # def get_serializer_class(self):
-    #     if self.action == "create":
-    #         return CriarItemSerializer
-    #     return ItemSerializer
