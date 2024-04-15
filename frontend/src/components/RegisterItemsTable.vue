66e01123363229a32e6d0c47b14e96dc5dcbfc81
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-04-15 14:34:16 -0300

    refactor: list item table

---
+++ frontend/src/components/RegisterItemsTable.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { useItemStore } from '@/stores/itemStore';
+
+const itemStore = useItemStore()
+itemStore.getItems()
+
+</script>
+
+<template>
+    <h1 class="text-center mt-5">Itens já registrados</h1>
+    <h1 v-if="itemStore.loading">Carregando itens</h1>
+    <v-table v-else>
+        <thead>
+            <tr>
+                <th>Item</th>
+                <th>Está na lista?</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="item in itemStore.registeredItems">
+                <td>{{ item.name }}</td>
+                <td v-if="item.active">Sim</td>
+                <td v-else>Não</td>
+            </tr>
+        </tbody>
+    </v-table>
+</template>
+
+<style scoped>
+* {
+    text-align: center;
+}
+</style>
