fb001d9aca080ae4ef937f6641796b57c435b048
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-04-19 21:58:59 -0300

    feat: get item data #21

--- frontend/src/stores/historyStore.js
+++ frontend/src/stores/historyStore.js
@@ -1,17 +1,19 @@
 import { ref, computed } from 'vue';
 import { defineStore } from 'pinia'
 
+import { useItemStore } from './itemStore';
 import HistoryApi from '@/api/history';
 
 export const useHistoryStore = defineStore('history', () => {
     const historyApi = new HistoryApi()
+    const itemStore = new useItemStore()
 
     const history = ref([])
     const loading = ref(false)
 
     async function getHistory() {
         loading.value = true
-        history.value.push(await historyApi.getHistory())
+        history.value = await historyApi.getHistory()
         loading.value = false
         return history.value
     }
@@ -19,6 +21,13 @@ export const useHistoryStore = defineStore('history', () => {
     getHistory()
 
     const registeredHistory = computed(() => (history.value))
+    const formatedHistory = computed(() =>
+        history.value.map(entry => ({
+            data: entry.date,
+            user: entry.user,
+            item: itemStore.getItemById(entry.id),
+        }))
+    )
 
-    return { getHistory, registeredHistory }
+    return { registeredHistory, formatedHistory }
 })
