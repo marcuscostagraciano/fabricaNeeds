4db797924f9167846f6ea52859127c7bc8012500
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-04-20 11:55:42 -0300

    feat: add authorization token

--- frontend/src/api/history.js
+++ frontend/src/api/history.js
@@ -1,8 +1,14 @@
 import axios from "axios";
 
+import { useAuthStore } from '@/stores/authStore';
+
+const authStore = useAuthStore()
+const token = await authStore.getToken()
+const config = { headers: { Authorization: `Bearer ${token}`, }, }
+
 export default class HistoryApi {
     async getHistory() {
-        const { data } = await axios.get(`/history/`)
+        const { data } = await axios.get(`/history/`, config)
         return data
     }
 }
