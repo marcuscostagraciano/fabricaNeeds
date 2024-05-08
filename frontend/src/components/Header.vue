2c975bfe0733b7eea6d64d7ce0f89671b65894fd
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-05-08 15:11:16 -0300

    Revert "feat: add logout button #16"
    This reverts commit 23a51a59af489af12a84de2a462ceee7c8ca4d25.
    maybe i like to revert commits /s

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -4,6 +4,7 @@ import { ref } from 'vue';
 const username = ref('')
 username.value = (new RegExp('(?:^|; )username=([^;]*)').exec(document.cookie)) ? (new RegExp('(?:^|; )username=([^;]*)').exec(document.cookie)[1]) : null;
 
+
 </script>
 
 <template>
@@ -15,12 +16,11 @@ username.value = (new RegExp('(?:^|; )username=([^;]*)').exec(document.cookie)) 
             <v-col id="site-title">
                 <v-app-bar-title>fabricaNeeds</v-app-bar-title>
             </v-col>
-            <v-col class="auth-options">
+            <v-col>
+                <!-- Placeholder for user operations (login and logout) -->
+                <!-- <v-app-bar-nav-icon icon="mdi-account" /> -->
                 <div v-if="username">
                     {{ username }}
-                    <br>
-                    <router-link to="/signin" id="logout-btn" @click="username = ''"
-                        class="rounded">Logout</router-link>
                 </div>
             </v-col>
         </v-row>
@@ -41,19 +41,7 @@ username.value = (new RegExp('(?:^|; )username=([^;]*)').exec(document.cookie)) 
     align-self: center;
 }
 
-.auth-options {
-    display: flex;
-    text-transform: capitalize;
-
-    align-content: center;
-    justify-content: center;
-    flex-direction: column;
-}
-
 #logout-btn {
     background-color: var(--palette-orange);
-    text-decoration: none;
-    color: #fff;
-    padding: 0rem 0.8rem;
 }
 </style>
