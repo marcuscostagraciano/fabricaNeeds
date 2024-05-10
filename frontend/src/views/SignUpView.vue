95524c0e065919102c4bbf74a6165603159e930f
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-05-10 10:38:50 -0300

    feat: add username field hint & message #33

--- frontend/src/views/SignUpView.vue
+++ frontend/src/views/SignUpView.vue
@@ -13,7 +13,7 @@ const userStore = useUserStore()
 const form = ref()
 const successful_signup = ref(false)
 const form_data = reactive({
-    name: '',
+    username: '',
     email: '',
     password: '',
     password_confirmation: '',
@@ -21,7 +21,7 @@ const form_data = reactive({
 
 const isFormValid = computed(() => {
     return [
-        requiredRule(form_data.name),
+        requiredRule(form_data.username),
         emailRule[1](form_data.email),
         passwordLengthRule[1](form_data.password),
         passwordConfirmationRule[1](form_data.password_confirmation, form_data.password)
@@ -36,7 +36,7 @@ async function createUser() {
     form.value.reset();
 
     // Clear input values
-    form_data.name = '';
+    form_data.username = '';
     form_data.email = '';
     form_data.password = '';
     form_data.password_confirmation = '';
@@ -46,7 +46,8 @@ async function createUser() {
 <template>
     <v-form class="form" @submit.prevent ref="form">
         <h1 class="text-center mb-5">Cadastro</h1>
-        <v-text-field label="Nome" :rules="[requiredRule]" v-model="form_data.name" type="text" />
+        <v-text-field label="Nome de Usuário" :rules="[requiredRule]" v-model="form_data.username" type="text"
+            messages="Este nome é vísivel para outros usuários." hint="Use o formato de NomeSobrenome." />
         <v-text-field label="E-mail" :rules="emailRule" v-model="form_data.email" type="email" />
         <v-text-field label="Senha" :rules="passwordLengthRule" v-model="form_data.password" type="password" />
         <v-text-field label="Confirmação de senha"
